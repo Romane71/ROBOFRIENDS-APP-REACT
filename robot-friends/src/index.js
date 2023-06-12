ac7f76591492a8138eccefa62ca89da06709f991
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-06-12 18:41:23 +0000

    first

---
+++ robot-friends/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Card from "./Card";
+import reportWebVitals from "./reportWebVitals";
+import "tachyons";
+
+ReactDOM.render(
+  <div>
+    <Card />
+    <Card />
+    <Card />
+  </div>,
+
+  document.getElementById("root")
+);
+
+reportWebVitals();
