d58b942bd1102b978ef724aeff2dee6614db1636
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-06-13 17:20:48 +0000

    fetching data

--- robot-friends/src/App.js
+++ robot-friends/src/App.js
@@ -8,10 +8,18 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
   }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  }
+
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
   };
@@ -22,13 +30,16 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1> RoboFriends </h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1> Loading...</h1>;
+    } else
+      return (
+        <div className="tc">
+          <h1> RoboFriends </h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
   }
 }
 
